f35f57ae6f972fc76bc9d73e5ffbb2c4476de70d
duddits <davinciihethal@gmail.com>
2025-07-17 17:15:33 +0300

    hello express

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,14 +1,15 @@
 let express = require('express');
 let app = express();
 
-const PORT = process.env.PORT || 3030;
-
-app.get("/", function (req, res) {
-  console.log('Hello World');
-  res.send('Hello Express');
+app.get("/", (req, res) => {
+  console.log("A request was made to /");
+  res.send("Hello World");
 });
 
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 
