32990d19ac4926cc2cdba1f5584a18ae0124c937
duddits <davinciihethal@gmail.com>
2025-07-17 13:20:36 +0300

    Check logs

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,7 +1,15 @@
 let express = require('express');
 let app = express();
+
 console.log("Hello World");
 
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 
 
