09b5c256be16876e3f9decbd84694eada06694d8
duddits <davinciihethal@gmail.com>
2025-07-17 17:40:12 +0300

    hello express

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello Express");
 });
 
 module.exports = app;
