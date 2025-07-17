8cff52bd13ab19e4b11376357a40058795d8f89b
duddits <davinciihethal@gmail.com>
2025-07-17 16:17:33 +0300

    hello express

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -3,18 +3,9 @@ let app = express();
 
 const PORT = process.env.PORT || 3030;
 
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <body>
-        <script>
-          console.log("Hello World");
-        </script>
-        <h1>Check the browser console</h1>
-      </body>
-    </html>
-  `);
-});
+function(req, res) {
+  res.send('Hello Express');
+}
 
 
 
