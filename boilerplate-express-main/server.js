9b0bbf41bbcf0d21c8d36285aedf3ad7082a31fa
duddits <davinciihethal@gmail.com>
2025-07-18 08:50:33 +0300

    log Hello World

--- boilerplate-express-main/server.js
+++ boilerplate-express-main/server.js
@@ -4,7 +4,7 @@
  * ***************************************************/
  
 const bGround = require('fcc-express-bground');
-const myApp = require('./myApp');
+const myApp = require('./app');
 const express = require('express');
 const app = express();
 
@@ -22,7 +22,7 @@ if (!process.env.DISABLE_XORIGIN) {
 }
 
 const port = process.env.PORT || 3000;
-bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, () => {
+bGround.setupBackgroundApp(app, app, __dirname).listen(port, () => {
   bGround.log(`Node is listening on port ${port}...`);
 });
 
