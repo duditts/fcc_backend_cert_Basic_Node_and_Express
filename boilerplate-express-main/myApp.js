40b2dabdb177aedfa3c68e8ca2d6556207166179
duddits <davinciihethal@gmail.com>
2025-07-18 11:47:15 +0300

    setting up static files

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let path = require('path');
- 
+
 //console.log("hello World");
 // app.get("/", (req, res) => {
 //    res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
   
 });
-
+app.use(express.static('public'));
 
 
 
