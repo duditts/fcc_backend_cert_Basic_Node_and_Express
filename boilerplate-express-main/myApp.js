6bcf6f7c9a59276777b213057e695299034809d2
duddits <davinciihethal@gmail.com>
2025-07-18 10:39:01 +0300

    serving index.html

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -5,7 +5,9 @@ console.log("hello World");
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
-
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 
 
