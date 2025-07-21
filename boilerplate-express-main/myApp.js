8ebf1604a1312a7e28e1c6f00ff69571684a4254
duddits <davinciihethal@gmail.com>
2025-07-21 12:50:28 +0300

    json response uppercase .env redo

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -9,10 +9,11 @@ app.use('/public', express.static(__dirname + '/public'));
 //    res.send("Hello Express");
 //});
 app.get('/json', (req ,res) => {
-    process.env.MESSAGE_STYLE == 'uppercase' ?
-        res.json({"message": "HELLO JSON"}) :
-        res.json({"message": "Hello json"});
-});
+  let message = process.env.MESSAGE_STYLE === 'uppercase' 
+    ?"HELLO JSON" 
+    :"Hello json";
+  res.json({"message":message  })});
+
 app.get('/',(req ,res) =>{
   res.sendFile(absolutePath);
 });
