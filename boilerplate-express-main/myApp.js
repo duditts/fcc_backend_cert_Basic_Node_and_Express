7ba5470b8453b35b24061b94162cc69aeca883a8
duddits <davinciihethal@gmail.com>
2025-07-21 11:12:47 +0300

    redo

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -2,39 +2,17 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 let path = require('path');
+let absolutePath = path.resolve(__dirname, 'views/index.html');
 
-//console.log("hello World");
-// app.get("/", (req, res) => {
-//    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-// });
-//app.use(express.static(path.join(__dirname, 'public', 'style.css')));
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  
-});
-// app.get('/json', function(req, res) {
-//   let message = "Hello json";
-//   if (process.env.MESSAGE_STYLE === "uppercase") {
-//     message = message.toUpperCase();
-//       }
-//   res.json({ message: message });
-// });
+console.log("hello World");
 
-//app.use(express.static(path.join)(__dirname, 'public','style.css'));
-
-app.get("/json", function(req, res) {
-  let message = "Hello json";
-
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-
-  res.json({ message: message });
+app.use('/', req ,res => {
+    res.send("Hello Express");
 });
 
-
-
+app.use(req ,res =>{
+    res.sendFile(absolutePath);
+});
 
 
 
