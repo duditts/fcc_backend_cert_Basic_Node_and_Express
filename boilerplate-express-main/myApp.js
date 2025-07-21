12e33b0bcb24d66ca3a22eeb9eab01c343160e07
duddits <davinciihethal@gmail.com>
2025-07-21 11:57:13 +0300

    serve static files

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -3,7 +3,7 @@ let express = require('express');
 let app = express();
 let path = require('path');
 let absolutePath = path.resolve(__dirname, 'views/index.html');
-express.static(__dirname + "/public");
+app.use(express.static(__dirname + "/public"));
 console.log("hello World");
 
 //app.get('/', (req ,res) => {
